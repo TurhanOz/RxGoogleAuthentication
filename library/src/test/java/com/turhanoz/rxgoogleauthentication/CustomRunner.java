ca902503ac1c3f43d8bec43de76d59a7732b608d
Turhan Oz <@turhanoz>
2015-10-16 18:19:45 +0200

    add ShadowGoogleAuthUtil & create a new Runner to include this new Shadow

---
+++ library/src/test/java/com/turhanoz/rxgoogleauthentication/CustomRunner.java
@@ -0,0 +1,29 @@
+package com.turhanoz.rxgoogleauthentication;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.turhanoz.rxgoogleauthentication.shadows.ShadowGoogleAuthUtil;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.internal.bytecode.InstrumentationConfiguration;
+
+//https://github.com/nenick/android-gradle-template/blob/master/appCt/src/test/java/com/example/project/robolectric/CostomRobolectricTestRunner.java
+public class CustomRunner extends RobolectricGradleTestRunner {
+    /**
+     * Creates a runner to run {@code testClass}. Looks in your working directory for your AndroidManifest.xml file
+     * and res directory by default. Use the {@link } annotation to configure.
+     *
+     * @param testClass the test class to be run
+     * @throws InitializationError if junit says so
+     */
+    public CustomRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+
+    public InstrumentationConfiguration createClassLoaderConfig() {
+        InstrumentationConfiguration.Builder builder = InstrumentationConfiguration.newBuilder();
+        builder.addInstrumentedClass(GoogleAuthUtil.class.getName());
+        return builder.build();
+    }
+}
